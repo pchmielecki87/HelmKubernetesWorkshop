a8f44f01886758179da25582103fa083eb937e11
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2025-05-05 12:02:46 +0200

    add mvc .net app

---
+++ Docker/DockerContainerizedApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DockerContainerizedApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
